b4b234d0dec19f15befe24227182eb9edf3cc3ab
Dex Hunter <dixingxu@gmail.com>
2017-01-24 20:03:04 +0800

    finish ch16

--- ex16_2.py
+++ ex16_2.py
@@ -1,13 +1,61 @@
 import time
-import datetime
-from datetime import date
-from datetime import time
+from datetime import date, time, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 def current_day():
-	return (date.today())
+	return (date.strftime(date.today(), "%A")) #strftime = string format time
+	
+def ObtainDate():
+	while True:
+		usrIn = input("Give a date in the format of yyyy-mm-dd: ")
+		try:
+			return datetime.strptime(usrIn, "%Y-%m-%d") # strptime = string parse time
+		except ValueError:
+			print ("Invalid format!\n")
 
 def usr_info(birthday):
-	'''something '''
+	'''Takes a birthday as input and prints the user's age and the number of days, hours, minutes and seconds until their next birthday
 
+	birthday: yyyy-MM-dd %Y-%m-%d
+	'''
+	today = datetime.today()
+	print ("Your age is: %d" % relativedelta(today, birthday).years)
+	#print ("Your age is: %d" % (today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day)))) #second way of writing
+	next_birthday = birthday.replace(year=today.year)
+	if next_birthday < today:
+		next_birthday = birthday.replace(year=today.year + 1)
+	duration = next_birthday - today
+	print ("Time until your next birthday is: ", duration.days, " days.")
+
+def double_day(bday1, bday2):
+	''' The author wrote "For two people born on different days, there is a day when one is twice as old as the other. That’s their Double Day."
+	But I do not know what does author mean by double day at all. So I looked up the answer and got the idea that double day is actually when the difference between birthday of first dude and second dude doubles. But I wonder if there are any meaning to calculate this.
+	source: https://github.com/AllenDowney/ThinkPython2/blob/master/code/double.py
+	
+	update: okay, I understand it now after testing. It basically means that older dude lives twice amount of days than younger dude
+	'''
+	old_dude = min(bday1, bday2)
+	young_dude = max(bday1, bday2)
+	delta = young_dude + (young_dude - old_dude)
+	return delta #return a timedelta object when old_dude lives twice amount of days as younger dude 
+	
+	
+def n_day(bd1, bd2, n):
+	'''A general version of double_day which computes the day when one person is n times older than the other.
+	
+	Return: timedelta
+	'''
+	old_dude = min(bd1, bd2)
+	young_dude = max(bd1, bd2)
+	delta = young_dude + n * (young_dude - old_dude)
+	return delta
+	
 if __name__ == '__main__':
+	print (date.today())
 	print (current_day())
+	birthday = ObtainDate()
+	usr_info(birthday)
+	# two day before I was born
+	rday = datetime(1995, 12, 21)
+	print("Double day is: ", double_day(rday, birthday))
+	print("Ten times day is: ", n_day(rday, birthday, 10))
--- test.py
+++ test.py
@@ -12,89 +12,185 @@ License: http://creativecommons.org/licenses/by/4.0/
 from __future__ import print_function, division
 
 
+from datetime import datetime
+
+# to avoid duplicating code, I'm importing everything from Time1
+#from Time1 import *
 class Time:
-    """Represents the time of day.
-       
-    attributes: hour, minute, second
-    """
+	"""Represents the time of day.
+	   
+	attributes: hour, minute, second
+	"""
 
 
 def print_time(t):
-    """Prints a string representation of the time.
+	"""Prints a string representation of the time.
 
-    t: Time object
-    """
-    print('%.2d:%.2d:%.2d' % (t.hour, t.minute, t.second))
+	t: Time object
+	"""
+	print('%.2d:%.2d:%.2d' % (t.hour, t.minute, t.second))
 
 
 def int_to_time(seconds):
-    """Makes a new Time object.
+	"""Makes a new Time object.
 
-    seconds: int seconds since midnight.
-    """
-    time = Time()
-    minutes, time.second = divmod(seconds, 60)
-    time.hour, time.minute = divmod(minutes, 60)
-    return time
+	seconds: int seconds since midnight.
+	"""
+	time = Time()
+	minutes, time.second = divmod(seconds, 60)
+	time.hour, time.minute = divmod(minutes, 60)
+	return time
 
 
 def time_to_int(time):
-    """Computes the number of seconds since midnight.
+	"""Computes the number of seconds since midnight.
 
-    time: Time object.
-    """
-    minutes = time.hour * 60 + time.minute
-    seconds = minutes * 60 + time.second
-    return seconds
+	time: Time object.
+	"""
+	minutes = time.hour * 60 + time.minute
+	seconds = minutes * 60 + time.second
+	return seconds
 
 
 def add_times(t1, t2):
-    """Adds two time objects.
+	"""Adds two time objects.
 
-    t1, t2: Time
+	t1, t2: Time
 
-    returns: Time
-    """
-    assert valid_time(t1) and valid_time(t2)
-    seconds = time_to_int(t1) + time_to_int(t2)
-    return int_to_time(seconds)
+	returns: Time
+	"""
+	assert valid_time(t1) and valid_time(t2)
+	seconds = time_to_int(t1) + time_to_int(t2)
+	return int_to_time(seconds)
 
 
 def valid_time(time):
-    """Checks whether a Time object satisfies the invariants.
+	"""Checks whether a Time object satisfies the invariants.
 
-    time: Time
+	time: Time
 
-    returns: boolean
-    """
-    if time.hour < 0 or time.minute < 0 or time.second < 0:
-        return False
-    if time.minute >= 60 or time.second >= 60:
-        return False
-    return True
+	returns: boolean
+	"""
+	if time.hour < 0 or time.minute < 0 or time.second < 0:
+		return False
+	if time.minute >= 60 or time.second >= 60:
+		return False
+	return True
+
+
+def is_after(t1, t2):
+	"""Returns True if t1 is after t2; false otherwise."""
+	return (t1.hour, t1.minute, t1.second) > (t2.hour, t2.minute, t2.second)
+
+
+def increment(t1, seconds):
+	"""Adds seconds to a Time object."""
+	assert valid_time(t1)
+	seconds += time_to_int(t1)
+	return int_to_time(seconds)
+
+
+def mul_time(t1, factor):
+	"""Multiplies a Time object by a factor."""
+	assert valid_time(t1)
+	seconds = time_to_int(t1) * factor
+	return int_to_time(seconds)
+
+
+def days_until_birthday(birthday):
+	"""How long until my next birthday?"""
+	today = datetime.today()
+	# when is my birthday this year?
+	next_birthday = datetime(today.year, birthday.month, birthday.day)
+
+	# if it has gone by, when will it be next year
+	if today > next_birthday:
+		next_birthday = datetime(today.year+1, birthday.month, birthday.day)
+
+	# subtraction on datetime objects returns a timedelta object
+	delta = next_birthday - today
+	return delta.days
+
+
+def double_day(b1, b2):
+	"""Compute the day when one person is twice as old as the other.
+
+	b1: datetime birthday of the younger person
+	b2: datetime birthday of the older person
+	"""
+	assert b1 > b2
+	delta = b1 - b2
+	print("delta:", delta)
+	dday = b1 + delta
+	return dday
+
+
+def datetime_exercises():
+	"""Exercise solutions."""
+
+	# print today's day of the week
+	today = datetime.today()
+	print(today.weekday())
+	print(today.strftime('%A'))
+
+	# compute the number of days until the next birthday
+	# (note that it usually gets rounded down)
+	birthday = datetime(1967, 5, 2)
+	print('Days until birthday', end=' ')
+	print(days_until_birthday(birthday))
+
+	# compute the day one person is twice as old as another
+	b1 = datetime(2006, 12, 27)
+	b2 = datetime(2006, 12, 25)
+	print('Double Day', end=' ')
+	print(double_day(b1, b2))
 
 
 def main():
-    # if a movie starts at noon...
-    noon_time = Time()
-    noon_time.hour = 12
-    noon_time.minute = 0
-    noon_time.second = 0
+	# if a movie starts at noon...
+	noon_time = Time()
+	noon_time.hour = 12
+	noon_time.minute = 0
+	noon_time.second = 0
 
-    print('Starts at', end=' ')
-    print_time(noon_time)
+	print('Starts at', end=' ')
+	print_time(noon_time)
 
-    # and the run time of the movie is 109 minutes...
-    movie_minutes = 109
-    run_time = int_to_time(movie_minutes * 60)
-    print('Run time', end=' ')
-    print_time(run_time)
+	# and the run time of the movie is 109 minutes...
+	movie_minutes = 109
+	run_time = int_to_time(movie_minutes * 60)
+	print('Run time', end=' ')
+	print_time(run_time)
 
-    # what time does the movie end?
-    end_time = add_times(noon_time, run_time)
-    print('Ends at', end=' ')
-    print_time(end_time)
+	# what time does the movie end?
+	end_time = add_times(noon_time, run_time)
+	print('Ends at', end=' ')
+	print_time(end_time)
+
+	print('Does it end after it begins?', end=' ')
+	print(is_after(end_time, noon_time))
+
+	print('Home by', end=' ')
+	travel_time = 600	  # 10 minutes
+	home_time = increment(end_time, travel_time)
+	print_time(home_time)
+
+	race_time = Time()
+	race_time.hour = 1
+	race_time.minute = 34
+	race_time.second = 5
+
+	print('Half marathon time', end=' ')
+	print_time(race_time)
+
+	distance = 13.1	   # miles
+	pace = mul_time(race_time, 1/distance)
+
+	print('Time per mile', end=' ')
+	print_time(pace)
+
+	datetime_exercises()
 
 
 if __name__ == '__main__':
-    main()
+	main()
