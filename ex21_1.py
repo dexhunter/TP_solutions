d0abae261eda1dca99043e17e8a1e614caad2140
Dex Hunter <dixingxu@gmail.com>
2017-02-23 17:50:43 +0800

    ch21

--- ex21_1.py
+++ ex21_1.py
@@ -3,14 +3,27 @@
 
 # 1. What is the order of growth of n^3 + n^2? What about 1000000*n^3 + n^2? What about n^3 + 1000000 * n^2?
 
-# The answer is cubic, O(x^3). When n approaches infinite, the constant and n^2 does not matter
+Ans: The answer is cubic, O(x^3). When n approaches infinite, the constant and n^2 does not matter
 
 # 2. What is the order of growth of n2 + n · n + 1 ? Before you start multiplying, remember that you only need the leading term.
 
-# cubic, O(x^3)
+Ans: cubic, O(x^3)
 
 3. If f is in O(g), for some unspecified function g , what can we say about af+b?
 
-If a is constant, I think the answer should be same as O(g)
+Ans: If a is constant, I think the answer should be same as O(g)
+
+4. If f1 and f2 are in O(g), what can we say about f1 + f2?
+
+
+Ans: 
+
+5. If f1 is in O(g) and f2 is in O(h), what can we say about f1 + f2?
+
+6. If f1 is in O(g) and f2 is in O(h), what can we say about f1 * f2?
+
+
+
+
 
 '''
--- ex21_2.py
+++ ex21_2.py
@@ -1,6 +1,25 @@
 '''
 1. What is a "comparison sort"? What is the best worst-case order of growth for a comparison sort? What is the best worst-case order of growth for any sort algorithm?
 
-Ans: comparison sort has a property that the sorting order it determined is based on the comprison result of its input elements
+Ans: All comparison sorts has a property that the sorting order it determined is based only on the comprisons between the input elements. (b) The best scenario is O(nlogn)  (c) For radix sort, the order of growth is 
+
+
+2. What is the order of growth of bubble sort, and does Barack Obama think it is "the wrong way to go"?
+
+
+Ans: The asymptotic complexity of bubble sort is O(n^2)
+
+
+3. What is a stable sort and why might it matter in practice?
+
+
+4. What is a stable sort and why might it matter in practice?
+
+
+5. What is the worst sorting algorithm (that has a name)?
+
+6. What sort algorithm does the C library use? What sort algorithm does Python use? Are these algorithms stable?
+
+7. Many of the non-comparison sorts are linear, so why does Python use an O(nlogn) comparison sort?
 
 '''
